fca6c8448dd11006d0a0b59e93b627e7609290c3
traplockvaults-eng <traplockvaults@gmail.com>
2025-09-20 01:06:03 +0000

    Your commit message

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,17 +1,18 @@
 /*
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
-import "forge-std/Script.sol";
-import "../src/AIFlashLoanExecutor.sol";
-
+import “forge-std/Script.sol”;
+import “…/src/AIFlashLoanExecutor.sol”;
 contract Deploy is Script {
-    function run() external {
-        uint256 pk = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(pk);
-        AIFlashLoanExecutor exec = new AIFlashLoanExecutor();
-        console2.log("AIFlashLoanExecutor:", address(exec));
-        vm.stopBroadcast();
-    }
+function run() external {
+address provider = vm.envAddress(“AAVE_V3_ADDRESSES_PROVIDER”);
+uint256 pk = vm.envUint(“PRIVATE_KEY”);
+Plain Text
+Copy
+    vm.startBroadcast(pk);
+    AIFlashLoanExecutor exec = new AIFlashLoanExecutor(IPoolAddressesProvider(provider));
+    console2.log("AIFlashLoanExecutor:", address(exec));
+    vm.stopBroadcast();
+}
 }
 */
